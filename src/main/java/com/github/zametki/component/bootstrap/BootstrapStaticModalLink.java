75bffb1643758b5e11758506ea83d5ff9fbc629b
Mikhail Fursov <mike.fursov@gmail.com>
2017-02-28 01:16:45 -0500

    Ссылки для модальных окон теперь имеют href=javascript(0)

--- src/main/java/com/github/zametki/component/bootstrap/BootstrapStaticModalLink.java
+++ src/main/java/com/github/zametki/component/bootstrap/BootstrapStaticModalLink.java
@@ -16,5 +16,6 @@ public class BootstrapStaticModalLink extends WebMarkupContainer {
 
         add(new StyleAppender("cursor: pointer;"));
         add(new AttributeModifier("onclick", "$('#" + modal.getDataTargetId() + "').modal();"));
+        add(new AttributeModifier("href", "javascript:void(0)"));
     }
 }
